def6cf987f182670ae2787405d6d2893965180ff
BikramBagchi <bikrambagchi1994@gmail.com>
2019-06-05 03:19:18 +0530

    Collection FrameWork

---
+++ src/CollectionFramework/ConceptOfCollectionFramework.java
@@ -0,0 +1,107 @@
+package CollectionFramework;
+
+public class ConceptOfCollectionFramework {
+
+	public static void main(String[] args) {
+		
+		/*
+		 Collection in java is a framework which is used 
+		 to store and manipulate the group of objects.
+		 
+		 Operation that we can perform using this framework are 
+		 Searching, Insertion , Manipulation , Deletion , sorting
+		 
+		 In collection different Interface are available 
+		 and they are ,
+		 
+		 Set , list, Queue and Dequeue
+		 
+		 Some important Class are 
+		 ArrayList
+		 Vector
+		 LinkedList
+		 Hash set
+		 Tree set
+		 Priority Queue
+		 
+		  
+		  This entire collection framework is available in JDK
+		  under one package and the package s called Java.util package 
+		  
+		**Collection is a interface in java
+		
+		How the framework work
+		
+		Interface Collection implements Itable (Interface or super super interface)
+		Now Three more interface and they are List , Set , Queue
+		Now list
+		
+		Collection is super class of List
+		
+		for list interface the class are 
+		
+		ArrayList implements List
+		LinkList implements List
+		Vector implements List
+		
+		Now there is on more class and that is Stack
+		but is the child class of Vector class so,
+		
+		Stack extends Vector
+		
+		 
+		
+		
+		Now Set 
+		
+	    Collection is super class of Set
+	    
+	    HashSet implements Set
+	    LinkedHashSet implements Set
+		
+	
+		
+		Now Queue
+		
+		Collection is super class of Queue
+		PriorityQueue extends Queue 
+		
+		We have two more interface and they are 
+		
+		Dequeue and SortedSet
+		
+		ArrayDeque implements Dequeue
+		LinkedList implements Dequeue
+		
+		Queue is the super interface of Dequeue
+		
+		TreeSet implements SortedSet
+		
+		SortedSet is the child interface of Set
+		
+		
+		We describe the relation between two interface by
+		Child Interface extends Parent Interface 
+		
+		
+		
+		
+		We describe relation between interface and class by
+		Class implements Interface 
+		For multiple interface we have 
+		Class implements Interface,Interface
+		 
+		
+		   
+		 
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		 */
+	}
+
+}
